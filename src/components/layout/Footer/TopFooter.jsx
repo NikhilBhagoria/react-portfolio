6a6d52bf26e4e893d8a6008513e8c0e927de7677
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2025-03-15 23:45:01 +0530

    update Footer

--- src/components/layout/Footer/TopFooter.jsx
+++ src/components/layout/Footer/TopFooter.jsx
@@ -57,7 +57,7 @@ const TopFooter = () => {
                 <svg className="h-5 w-5 mr-2 text-[var(--color-accent)]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                 </svg>
-                <a href="mailto:nikhilkumar2450@gmail.com?subject=Inquiry%20from%20Portfolio">
+                <a href="mailto:nikhilkumar2450@gmail.com?subject=Inquiry%20from%20">
                   nikhilkumar2450@gmail.com
                 </a>
               </li>
